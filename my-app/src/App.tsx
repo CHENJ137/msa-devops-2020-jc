bc14e823a86404f3ee739bc928a56165a02f1b7c
CHENJ137 <key2chan@gmail.com>
2020-07-21 01:56:53 +1200

    updated website

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Jian's MSA DevOps 2020 phase 1 work.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://abnormalperks.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Abnormal Perks
         </a>
       </header>
     </div>
